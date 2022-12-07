c18852b2da86a3d0c40392d5a34eb9397cc5b612
Manuel <m.ebneter@delight.ch>
2022-12-07 13:31:23 +0100

    first commit

---
+++ src/shared_components/spinner.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="q-gutter-md row items-center">
+      <q-spinner-cube
+        v-for="size in ['xs', 'sm', 'md', 'lg', 'xl']"
+        :key="size"
+        :size="size"
+        color="primary"
+      />
+    </div>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "SpinnerFun"
+});
+</script>
